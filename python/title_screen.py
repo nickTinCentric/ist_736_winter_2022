88ea6571aafd722f296237274ecc2041f8316d6d
Nick Tinsley <nicktinsley@microsoft.com>
2022-01-10 11:37:24 -0600

    initial commit

---
+++ python/title_screen.py
@@ -0,0 +1,18 @@
+def load_page():
+    print("*****************************************************************")
+    print("*** This is the title screen of the app                       ***")
+    print("*** by Nick Tinsley                                           ***")
+    print("*****************************************************************")
+    print("")
+
+
+
+load_page()
+
+
+x = 1 + 2 * 3 - 8 / 4
+
+print(x)
+
+x = int(98.6)
+print(x)
